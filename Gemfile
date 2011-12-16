49b11b11cafd989c3fcbad8c7ca2d084211bc7bc
Dave Lyon and Joshua Davey <dev+davelyon+jgdavey@hashrocket.com>
2011-12-16 15:45:59 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'nokogiri'
+gem 'rest-client'
+gem 'pry'
---
+++ scraper.rb
@@ -0,0 +1,45 @@
+require 'bundler'
+Bundler.require
+
+class Scraper
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def process(url)
+  end
+
+  def get_content_from(url)
+    RestClient.get(url)
+  end
+end
+
+class ConfreaksScraper < Scraper
+  SITE_URI = "http://confreaks.net"
+
+  def process
+    content = get_content_from(url)
+    videos_doc= Nokogiri::HTML.parse(content)
+    links = videos_doc.xpath('//*[@class="video"]//*[@class="title"]/a')
+    video_data = []
+    links.each_with_index do |link|
+      vid = {}
+      show_page_url = SITE_URI + link.attr('href')
+      show_page_content = RestClient.get(show_page_url)
+      show_page_doc = Nokogiri::HTML.parse(show_page_content)
+
+      p "Sraping: #{show_page_url}"
+
+      title = show_page_doc.xpath('.//*[@class="video-title"]').text.strip
+      vid[:title] = title
+      vid[:uri] = show_page_url
+      vid[:presenters] = show_page_doc.xpath('.//*[@class="video-presenters"]/a').map(&:text)
+      vid[:description] = show_page_doc.xpath('.//*[@class="video-abstract"]/p').text.strip
+      video_data << vid
+    end
+
+    video_data
+  end
+end
